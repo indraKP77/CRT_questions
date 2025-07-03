abb83e8bd6d5fb3eac874b7afe4c1369e37f1b27
Indra Kumar <indravip3@gmail.com>
2025-07-03 15:44:41 +0530

    missingElement

---
+++ missingElement.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n, 0);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cin >> nums[i];
+    }
+    int miss = 0;
+    sort(nums.begin(), nums.end());
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] - nums[i - 1] != 1)
+        {
+            miss = nums[i - 1] + 1;
+        }
+    }
+    cout << miss << endl;
+    return 0;
+}
