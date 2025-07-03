9b33da0a31c9e5eca7254eacc6a9b77d5a98fcb3
Indra Kumar <indravip3@gmail.com>
2025-07-03 14:46:29 +0530

    maarpu chendina vaade manishi

--- rotatedArray.cpp
+++ rotatedArray.cpp
@@ -6,10 +6,9 @@ using namespace std;
 void rightRotate(vector<int>& arr, int d) {
     int n = arr.size();
     d = d % n;
-
+    reverse(arr.end() - d, arr.end());
+    reverse(arr.begin(), arr.end() - d);
     reverse(arr.begin(), arr.end());
-    reverse(arr.begin(), arr.begin() + d);
-    reverse(arr.begin() + d, arr.end());
 }
 
 int main() {
