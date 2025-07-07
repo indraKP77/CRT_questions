5cef7e0a38dc087d16df4aee41a2f802f49c9664
Indra Kumar <indravip3@gmail.com>
2025-07-07 12:10:32 +0530

    Reverse LL and cycle detection

--- LL.cpp
+++ LL.cpp
@@ -34,14 +34,49 @@ class List{
             temp = temp->next;
         }
     }
-};
+    void reverse() {
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* nex = NULL;
 
+    while (curr != NULL) {
+        nex = curr->next;   
+        curr->next = prev; 
+        prev = curr;       
+        curr = nex;         
+    }
+
+    head = prev;  
+}
+
+bool cycle(){
+    Node* slow = head;
+    Node* fast = head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            return true;
+        }
+    }
+    return false;
+}
+
+};
 int main(){
     List l1;
     l1.insert(1);
     l1.insert(2);
     l1.insert(3);
     l1.insert(4);
+    l1.reverse();
     l1.display();
+    bool isCycle = l1.cycle();
+    if(isCycle){
+        cout<<"\nThere is a cycle"<<endl;
+    }
+    else{
+        cout<<"\nNo cycle"<<endl;
+    }
     return 0;
 }
