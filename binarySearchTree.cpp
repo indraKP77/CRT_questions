aa6f3c596166f1eec22d4c2880214d85d3a90b2c
Indra Kumar <indravip3@gmail.com>
2025-07-14 12:07:32 +0530

    binaty search tree

---
+++ binarySearchTree.cpp
@@ -0,0 +1,146 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        right = left = NULL;
+    }
+};
+
+Node* insert(Node* root,int val){
+    if(root == NULL){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left = insert(root->left,val);
+    }
+    else{
+        root->right = insert(root->right,val);
+    }
+    return root;
+}
+
+void inorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+bool search(Node* root,int key){
+    if(root == NULL){
+        return false;
+    }
+    if(root->data == key){
+        return true;
+    }
+    if(key<root->data){
+        return search(root->left,key);
+    }
+    else{
+        return search(root->right,key);
+    }
+}
+
+void levelOrder(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(q.size()>0){
+        Node* curr = q.front();
+        q.pop();
+        if(curr == NULL){
+            if(!q.empty()){
+                cout<<endl;
+                q.push(NULL);
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+        cout<<curr->data<<" ";
+        if(curr->left!=NULL){
+            q.push(curr->left);
+        }
+        if(curr->right!=NULL){
+            q.push(curr->right);
+        }
+    }
+}
+
+Node* buildBST(vector<int>& arr){
+    Node* root = NULL;
+    for(int data:arr){
+        root = insert(root,data);
+    }
+    return root;
+}
+
+int heightOfTree(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int lh = heightOfTree(root->left);
+    int rh = heightOfTree(root->right);
+    return 1 + max(lh,rh);
+}
+
+Node* getIS(Node* root){
+    while(root!=NULL && root->left!=NULL){
+        root = root->left;
+    }
+    return root;
+}
+
+Node* delNode(Node* root,int key){
+    if(root == NULL){
+        return NULL;
+    }
+    if(key<root->data){
+        root->left = delNode(root->left,key);
+    }
+    else if(key>root->data){
+        root->right = delNode(root->right,key);
+    }
+    else{
+        if(root->left == NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if(root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        else{
+            Node* IS = getIS(root->right);
+            root->data = IS->data;
+            root->right = delNode(root->right,IS->data);
+        }
+    }
+    return root;
+}
+
+int main(){
+    vector<int> arr = {27,14,35,10,19,31,42};
+    Node* root =  buildBST(arr);
+    cout<<endl;
+    inorder(root);
+    cout<<endl;
+    int height = heightOfTree(root);
+    cout<<"Height of Tree "<<height<<endl;
+    cout<<"Level Order "<<endl;
+    levelOrder(root);
+    return 0;
+}
