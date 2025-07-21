0dc4871c90b44d4234bddbb2d79b77dd783ed9e2
Indra Kumar <indravip3@gmail.com>
2025-07-21 09:35:55 +0530

    commit message

--- stringSort.cpp
+++ stringSort.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-int findMax(unordered_map<int,int>& mpp){
-    int max = 0;int maxPos = 0;
-    for(auto it: mpp){
-        if(it.second>max){
-            max = it.second;
+// Find key with max value in map and remove it
+int findMax(unordered_map<int, int>& mpp) {
+    int maxVal = -100;
+    int maxPos = -1;
+    for (auto it : mpp) {
+        if (it.second > maxVal) {
+            maxVal = it.second;
             maxPos = it.first;
         }
     }
@@ -15,27 +17,68 @@ int findMax(unordered_map<int,int>& mpp){
     return maxPos;
 }
 
-int main(){
-    vector<string> str;
-    int n;
-    cout<<"Enter no. of strings"<<endl;
-    cin>>n;
-    string s;
-    for(int i = 0;i < n;i++){
-        cin>>s;
-        str.push_back(s);
+// Find key with min value in map and remove it
+int findMin(unordered_map<int, int>& mpp) {
+    int minVal = 100;
+    int minPos = -1;
+    for (auto it : mpp) {
+        if (it.second < minVal) {
+            minVal = it.second;
+            minPos = it.first;
+        }
     }
-    unordered_map<int,int>  mpp;
-    for(int i = 0;i < str.size();i++){
-        mpp[i] = str[i].size();
+    mpp.erase(minPos);
+    return minPos;
+}
+
+// Sort strings based on first character
+void sortByFirst(const vector<string>& str) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < str.size(); i++) {
+        mpp[i] = str[i][0] - 'a';  // assumes lowercase letters
     }
     vector<string> str1;
-    for(int i = 0;i < n;i++){
-        int max = findMax(mpp);
-        str1.push_back(str[max]);
+    for (int i = 0; i < str.size(); i++) {
+        int minIdx = findMin(mpp);
+        str1.push_back(str[minIdx]);
     }
-    for(int i = 0;i < n;i++){
-        cout<<str1[i]<<" "<<endl;
+
+    cout << "\nStrings sorted by first character:\n";
+    for (const auto& s : str1) {
+        cout << s << endl;
     }
+}
+
+int main() {
+    vector<string> str;
+    int n;
+    cout << "Enter number of strings: ";
+    cin >> n;
+
+    string s;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        str.push_back(s);
+    }
+
+    // Sort by length using unordered_map
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < str.size(); i++) {
+        mpp[i] = str[i].size();
+    }
+
+    vector<string> str1;
+    for (int i = 0; i < n; i++) {
+        int maxIdx = findMax(mpp);
+        str1.push_back(str[maxIdx]);
+    }
+
+    cout << "\nStrings sorted by length (descending):\n";
+    for (const auto& s : str1) {
+        cout << s << endl;
+    }
+
+    sortByFirst(str); // You can also call sortByFirst(str1) if you want to sort by first char after length sort
+
     return 0;
 }
